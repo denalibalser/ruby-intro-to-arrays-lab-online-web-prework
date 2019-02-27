cfbcacd0d6afd14747b39473c757958e9b830ce1
Denali Balser <denali.balser167@gmail.com>
2019-02-27 01:14:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -27,7 +27,7 @@ def first_element_with_array_methods(array)
   south_east_asia.shift
 end
 
-def length_of_array(array)
-  first_country = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-  first_country.length_of_array
+def last_element_with_array_methods_of_array(array)
+  last_country = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
+  first_country.push
 end
