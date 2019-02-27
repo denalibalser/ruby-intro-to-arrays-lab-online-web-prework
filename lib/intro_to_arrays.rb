caa94e20a5dcefe2ca0a797e5c37e2ec5043c1d1
Denali Balser <denali.balser167@gmail.com>
2019-02-27 01:19:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -31,3 +31,8 @@ def last_element_with_array_methods(array)
   last_country = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
   last_country.last
 end
+
+def length_of_array(array)
+  programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
+  programming_languages.length
+end 
